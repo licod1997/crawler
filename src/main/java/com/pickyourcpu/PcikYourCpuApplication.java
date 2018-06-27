10120466f8d806668c8ef69d8d4db25641e67bac
licod1997 <licod1997@gmail.com>
2018-06-28 02:31:44 +0900

    Initial commit

---
+++ src/main/java/com/pickyourcpu/PcikYourCpuApplication.java
@@ -0,0 +1,12 @@
+package com.pickyourcpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcikYourCpuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcikYourCpuApplication.class, args);
+	}
+}
